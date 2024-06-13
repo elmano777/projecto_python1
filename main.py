f71c25c93615bdfeabdfeb61ba08759bb5f6c9ae
Rola D. <97373698+elmano777@users.noreply.github.com>
2024-06-13 00:20:04 -0500

    todo9

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ stop_words = set(stopwords.words('english'))
 palabras_inutiles = stop_words.union({
     'power', 'accuracy', 'user', 'target', 'pp', 'moves', 'the', 'to', 'is', 'on', 'by', 'pokemon', 'battle',
     'spot', 'singles', 'doubles', 'gen', 'other', 'gens', "ability", "abilities", '10', '20'
-    '30', '40', '50', '60', '70', '80', '90', '100'
+    '30', '40', '50', '60', '70', '80', '90', '100',
 })
 
 cluster_nombres = {
